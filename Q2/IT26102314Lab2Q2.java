8f650abb1fa9beb9dca77beaf869816735810ab3
IT26102314 <nethminiherath189@gmail.com>
2026-06-27 14:08:55 +0530

    Add files via upload
    upload IT26102314Lab2Q2.java

---
+++ Q2/IT26102314Lab2Q2.java
@@ -0,0 +1,12 @@
+public class IT26102314Lab2Q2 {
+  
+public static void main(String[] args) {
+    double length = 10;
+    double perimeterOfSquare = 4 * length;
+    double PI = 3.14;
+    
+    double radius = perimeterOfSquare / (2 * PI);
+        
+    System.out.println("Radius of the circular fence: " + radius);
+    }
+}
